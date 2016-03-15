e97909360f34fa328fbcfd1a8b38fd138dde4dff
Shiva Upadhye <shivaa03@yahoo.com>
2016-03-15 12:26:05 -0700

    Modified YearMenu and and created a new class TestUI.

---
+++ src/TestUI.java
@@ -0,0 +1,96 @@
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+
+import javax.swing.BoxLayout;
+import javax.swing.JCheckBox;
+import javax.swing.JCheckBoxMenuItem;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextField;
+
+public class TestUI {
+
+	public static void main(String[] args){
+		
+		//Reads data from CSV file and parses it
+	    final String FILENAME = "resources\\cellular.csv";   
+        CSVReader parser = new CSVReader(FILENAME);
+        String [] countryNames = parser.getCountryNames();
+        int [] yearLabels = parser.getYearLabels();
+        double [][] parsedTable = parser.getParsedTable(); 
+		
+        
+
+       //creates the main frame
+        JFrame UI_menu = new JFrame();
+        UI_menu.setTitle("Graph Settings");
+        UI_menu.setLayout(new BorderLayout());
+        UI_menu.setSize(600,600);
+     
+        JPanel yearMenu_panel = new JPanel();
+        yearMenu_panel.setLayout(new BoxLayout(yearMenu_panel, BoxLayout.Y_AXIS));
+        yearMenu_panel.setSize(new Dimension(300,600));
+        
+       YearMenu startYearMenu = new YearMenu(250,600, yearLabels);
+        JLabel prompt1 = new JLabel("Select starting year ");
+        yearMenu_panel.add(prompt1);
+        yearMenu_panel.add(startYearMenu);
+       
+        YearMenu endYearMenu = new YearMenu(250,600,yearLabels);
+        JLabel prompt2 = new JLabel("Select ending year ");
+        yearMenu_panel.add(prompt2);
+        yearMenu_panel.add(endYearMenu);
+        
+        UI_menu.add(yearMenu_panel); 
+      
+        JPanel countryMenu_panel = new JPanel();
+        countryMenu_panel.setLayout(new BoxLayout(countryMenu_panel, BoxLayout.Y_AXIS));
+        countryMenu_panel.setSize(new Dimension(300,600));
+        
+        JLabel promptLabel = new JLabel("Select countries to graph");
+        countryMenu_panel.add(promptLabel);
+        
+        for(int index = 0; index < countryNames.length; index++){
+        	CountryMenuItem item = new CountryMenuItem(countryNames[index],false);
+        	countryMenu_panel.add(item);
+        }
+        
+    
+       JScrollPane Scroller = new JScrollPane(countryMenu_panel);
+       Scroller.setPreferredSize(new Dimension(300,500));
+        UI_menu.add(Scroller, BorderLayout.WEST);
+        
+        
+/**        JPanel countryMenu_Panel = new JPanel();
+        countryMenu_Panel.setSize(200,600);
+        for(int index = 0; index < countryNames.length; index++){
+        	CountryItem newItem = new CountryItem(countryNames[index]);
+        	countryMenu_Panel.add(newItem);
+        }
+        
+        UI_menu.add(countryMenu_Panel);
+        
+
+   
+//        CountryMenu countryList = new CountryMenu(countryNames);
+//        leftPanel.add(countryList);
+//        UI_menu.add(leftPanel, BorderLayout.WEST);
+
+//creates a panel for year menu
+//        JPanel yearPanel = new JPanel();
+//        YearMenu startYear_menu = new YearMenu(300,600,yearLabels);
+//        JLabel prompt1 = new JLabel("Select Starting Year");
+//        startYear_menu.add(prompt1);
+//        yearPanel.add(startYear_menu, BorderLayout.NORTH);	
+
+	*/ 
+        UI_menu.setVisible(true);
+	}
+	
+}
--- src/YearMenu.java
+++ src/YearMenu.java
@@ -3,31 +3,37 @@ import java.awt.event.ItemListener;
 
 import javax.swing.JComboBox;
 import javax.swing.JPanel;
-
+import javax.swing.ListSelectionModel;
+/**
+ * One object of class YearMenu creates a drop down menu of years.
+ * @author Shiva
+ *
+ */
 public class YearMenu extends JPanel implements ItemListener{
 	
-	private JComboBox <Integer> year_menu = new JComboBox<>();
-	private int selectedYear = 0;
+	private JComboBox <Integer> year_menu;
+	private int selectedYear;
 	
 	public YearMenu(int width, int height, int [] years){
 		this.setSize(width, height);
+		this.year_menu =  new JComboBox<>();
 		for(int index = 0; index < years.length; index++){
 			year_menu.addItem(years[index]);
 		}
 		this.add(year_menu);
-		year_menu.setSelectedIndex(0);
-		year_menu.addItemListener(this);
-	}
-
-	public int getSelectedYear(){
-		return selectedYear;
+		year_menu.addItemListener(this); 
 	}
 
 	@Override
 	public void itemStateChanged(ItemEvent evt) {
-		if(evt.getStateChange()==ItemEvent.SELECTED);
-		selectedYear = (Integer)year_menu.getSelectedItem();
+		if(evt.getStateChange()==ItemEvent.SELECTED){
+			selectedYear = (Integer) year_menu.getSelectedItem();
+		}
 		
+	}
+	
+	public int getSelectedYear(){
+		return selectedYear;
 	}
 
 }
